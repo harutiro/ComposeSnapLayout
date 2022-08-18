58237e0406fbb5c809524a1f43e82389ed68d77f
harutiro <harutiro2027@outlook.jp>
2022-08-19 03:08:11 +0900

    update:ただのRowを完成

--- app/src/main/java/net/harutiro/composesnaplayout/MainActivity.kt
+++ app/src/main/java/net/harutiro/composesnaplayout/MainActivity.kt
@@ -3,13 +3,15 @@ package net.harutiro.composesnaplayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.harutiro.composesnaplayout.ui.theme.ComposeSnapLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +24,56 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainContent(placeholderItems = mutableListOf("hoge","hoge","huga"))
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainContent(
+    placeholderItems: MutableList<String>
+) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            text = "Example Horizontal LazyRow"
+        )
+
+        LazyRow(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            items(items = placeholderItems) { itemMessage: String ->
+                Card(
+                    modifier = Modifier
+                        .width(350.dp)
+                        .height(350.dp)
+                        .padding(16.dp)
+                ) {
+                    //Put text or whatever here
+
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeSnapLayoutTheme {
-        Greeting("Android")
+        MainContent(placeholderItems = mutableListOf("hoge","hoge","huga"))
     }
 }
